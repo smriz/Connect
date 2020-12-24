5ebed40d12c36b23cb0bc22a5755f3957d23df65
smriz <smohammedrizwan98@gmail.com>
2020-12-24 14:18:46 +0530

    chat functionality added

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,20 +1,24 @@
-const mongoose = require('mongoose'); //mongoose
-const DB_URI = 'mongodb://localhost:27017/chatapplication' // our local uri for MongoDB
+const mongoose = require("mongoose"); //mongoose
+const DB_URI =
+  "mongodb+srv://master:master123@cluster1.luv3e.mongodb.net/xrosstalk?retryWrites=true&w=majority"; // our temp uri for MongoDB
 
-mongoose.connect(DB_URI, {
+mongoose
+  .connect(DB_URI, {
     //To fix all deprecation warnings, follow the below steps:
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
-}).then(() => {
+    useFindAndModify: false,
+  })
+  .then(() => {
     //successfully connected to database
-    console.log("DB connected to : " + DB_URI)
-}).catch((err) => {
+    console.log("DB connected to : " + DB_URI);
+  })
+  .catch((err) => {
     //fail to connect database and print error
-    console.log("DB failed :  " + DB_URI + " : " + err)
-});
+    console.log("DB failed :  " + DB_URI + " : " + err);
+  });
 
 module.exports = {
-    mongoose
+  mongoose,
 };
